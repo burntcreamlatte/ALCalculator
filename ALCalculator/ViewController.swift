d48988e877815c0573723b1c88bfef0196c3ffda
Aaron Shackelford <burntcreamlatte@icloud.com>
2019-12-16 11:53:34 -0700

    Initial Commit

---
+++ ALCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALCalculator
+//
+//  Created by Aaron Shackelford on 12/16/19.
+//  Copyright © 2019 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
